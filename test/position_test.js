4fac72169c7a33193b5159c64d7c6082e3660b5a
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-14 20:35:08 +0800

    Give moving functionality to position

--- test/position_test.js
+++ test/position_test.js
@@ -2,17 +2,48 @@ import assert from 'assert'
 import Position from '../lib/position.js'
 
 describe('Position', () => {
-  describe('coordinates', () => {
+  it('does not save negative numbers', () => {
+    const position = new Position({ x: -1, y: 2 }) 
+    assert.deepEqual(position.coordinates(), { x: 0, y: 2 })
+
+    position.moveLeft()
+
+    assert.deepEqual(position.coordinates(), { x: 0, y: 2 })
+  })
+
+  describe('#coordinates', () => {
     it('returns object with x and y', () => {
       const coordinates = { x: 1, y: 2 }
       const position = new Position(coordinates) 
-      assert.equal(coordinates.x, position.coordinates().x)
-      assert.equal(coordinates.y, position.coordinates().y)
+      assert.deepEqual(coordinates, position.coordinates())
     })
   })
 
-  describe('moveUp', () => {})
-  describe('moveLeft', () => {})
-  describe('moveRight', () => {})
-  describe('moveDown', () => {})
+  describe('#moveUp', () => {
+    it('reduces Y by a step', () => {
+      const position = new Position({ x: 1, y: 2 }) 
+      assert.deepEqual(position.moveUp(), { x: 1, y: 1 })
+    }) 
+  })
+
+  describe('#moveDown', () => {
+    it('increments Y by a step', () => {
+      const position = new Position({ x: 1, y: 2 }) 
+      assert.deepEqual(position.moveDown(), { x: 1, y: 3 })
+    }) 
+  })
+
+  describe('#moveLeft', () => {
+    it('reduces X by a step', () => {
+      const position = new Position({ x: 1, y: 2 }) 
+      assert.deepEqual(position.moveLeft(), { x: 0, y: 2 })
+    }) 
+  })
+
+  describe('#moveRight', () => {
+    it('increments X by a step', () => {
+      const position = new Position({ x: 1, y: 2 }) 
+      assert.deepEqual(position.moveRight(), { x: 2, y: 2 })
+    }) 
+  })
 })
