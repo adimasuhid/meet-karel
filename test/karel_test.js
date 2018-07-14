038b1d549c7551551cc8ed9b4fe7e78432fcc56b
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-14 20:49:45 +0800

    Update karel to use Position

--- test/karel_test.js
+++ test/karel_test.js
@@ -2,64 +2,40 @@ import assert from 'assert'
 import Karel from '../lib/karel.js'
 
 describe('Karel', () => {
-  describe('#constructor', () => {
-    it('sets direction', () => {
-      const karel = new Karel({ x: 1, y: 2, direction: 'N' })
-
-      assert.equal(karel.direction, 'N')
-    })
-
-    it('sets x', () => {
-      const karel = new Karel({ x: 1, y: 2, direction: 'N' })
-
-      assert.equal(karel.x, 1)
-    })
-
-    it('sets y', () => {
-      const karel = new Karel({ x: 1, y: 2, direction: 'N' })
-
-      assert.equal(karel.y, 2)
-    })
-  })
-
   describe('#move', () => {
     it('does not change direction', () => {
       const karel = new Karel({ x: 1, y: 2, direction: 'N' })
       karel.move() 
 
-      assert.equal(karel.direction, 'N')
+      assert.equal(karel.direction(), 'N')
     })
 
     it('moves a step north given N', () => {
       const karel = new Karel({ x: 1, y: 2, direction: 'N' })
       karel.move() 
 
-      assert.equal(karel.y, 1)
-      assert.equal(karel.x, 1)
+      assert.deepEqual(karel.coordinates(), { x: 1, y: 1 })
     })
 
     it('moves a step south given S', () => {
       const karel = new Karel({ x: 1, y: 2, direction: 'S' })
       karel.move() 
 
-      assert.equal(karel.y, 3)
-      assert.equal(karel.x, 1)
+      assert.deepEqual(karel.coordinates(), { x: 1, y: 3 })
     })
 
     it('moves a step west given W', () => {
       const karel = new Karel({ x: 1, y: 2, direction: 'W' })
       karel.move() 
 
-      assert.equal(karel.y, 2)
-      assert.equal(karel.x, 0)
+      assert.deepEqual(karel.coordinates(), { x: 0, y: 2 })
     })
 
     it('moves a step west given E', () => {
       const karel = new Karel({ x: 1, y: 2, direction: 'E' })
       karel.move() 
 
-      assert.equal(karel.y, 2)
-      assert.equal(karel.x, 2)
+      assert.deepEqual(karel.coordinates(), { x: 2, y: 2 })
     })
   })
 
@@ -68,22 +44,21 @@ describe('Karel', () => {
       const karel = new Karel({ x: 1, y: 2, direction: 'N' })
       karel.turnLeft()
 
-      assert.equal(karel.x, 1)
-      assert.equal(karel.y, 2)
+      assert.deepEqual(karel.coordinates(), { x: 1, y: 2 })
     })
 
     it('turns west given N', () => {
       const karel = new Karel({ x: 1, y: 2, direction: 'N' })
       karel.turnLeft()
 
-      assert.equal(karel.direction, 'W')
+      assert.equal(karel.direction(), 'W')
     })
 
     it('turns east given S', () => {
       const karel = new Karel({ x: 1, y: 2, direction: 'S' })
       karel.turnLeft()
 
-      assert.equal(karel.direction, 'E')
+      assert.equal(karel.direction(), 'E')
     })
   })
 
@@ -92,22 +67,21 @@ describe('Karel', () => {
       const karel = new Karel({ x: 1, y: 2, direction: 'N' })
       karel.turnRight()
 
-      assert.equal(karel.x, 1)
-      assert.equal(karel.y, 2)
+      assert.deepEqual(karel.coordinates(), { x: 1, y: 2 })
     })
 
     it('turns west given N', () => {
       const karel = new Karel({ x: 1, y: 2, direction: 'N' })
       karel.turnRight()
 
-      assert.equal(karel.direction, 'E')
+      assert.equal(karel.direction(), 'E')
     })
 
     it('turns east given E', () => {
       const karel = new Karel({ x: 1, y: 2, direction: 'E' })
       karel.turnRight()
 
-      assert.equal(karel.direction, 'S')
+      assert.equal(karel.direction(), 'S')
     })
   })
 
