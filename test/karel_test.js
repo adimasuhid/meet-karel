614c88c58614a0c498614bc6c800fbe45126e216
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-14 16:39:15 +0800

    Add move

--- test/karel_test.js
+++ test/karel_test.js
@@ -8,5 +8,58 @@ describe('Karel', () => {
 
       assert.equal(karel.direction, 'N')
     })
+
+    it('sets x', () => {
+      const karel = new Karel({ x: 1, y: 2, direction: 'N' })
+
+      assert.equal(karel.x, 1)
+    })
+
+    it('sets y', () => {
+      const karel = new Karel({ x: 1, y: 2, direction: 'N' })
+
+      assert.equal(karel.y, 2)
+    })
+  })
+
+  describe('#move', () => {
+    it('does not change direction', () => {
+      const karel = new Karel({ x: 1, y: 2, direction: 'N' })
+      karel.move() 
+
+      assert.equal(karel.direction, 'N')
+    })
+
+    it('moves a step north given N', () => {
+      const karel = new Karel({ x: 1, y: 2, direction: 'N' })
+      karel.move() 
+
+      assert.equal(karel.y, 1)
+      assert.equal(karel.x, 1)
+    })
+
+    it('moves a step south given S', () => {
+      const karel = new Karel({ x: 1, y: 2, direction: 'S' })
+      karel.move() 
+
+      assert.equal(karel.y, 3)
+      assert.equal(karel.x, 1)
+    })
+
+    it('moves a step west given W', () => {
+      const karel = new Karel({ x: 1, y: 2, direction: 'W' })
+      karel.move() 
+
+      assert.equal(karel.y, 2)
+      assert.equal(karel.x, 0)
+    })
+
+    it('moves a step west given E', () => {
+      const karel = new Karel({ x: 1, y: 2, direction: 'E' })
+      karel.move() 
+
+      assert.equal(karel.y, 2)
+      assert.equal(karel.x, 2)
+    })
   })
 })
