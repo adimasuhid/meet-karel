3e14bad0096158dc5e2aab4254987cefe1bc6af4
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-15 19:12:38 +0800

    Environment creates walls

--- test/environment_test.js
+++ test/environment_test.js
@@ -8,5 +8,20 @@ describe('Environment', () => {
 
       assert.equal(environment.boulders().length, 3)
     })
+
+
+    describe('#walls', () => {
+      it('creates 8 walls given a 3x3 board', () => {
+        const environment = new Environment({ width: 3, height: 3, boulderCount: 1 }) 
+        
+        assert.equal(environment.walls().length, 8)
+      })
+    
+      it('creates 14 walls given a 5x4 board', () => {
+        const environment = new Environment({ width: 5, height: 4, boulderCount: 1 }) 
+        
+        assert.equal(environment.walls().length, 14)
+      })
+    })
   })
 })
