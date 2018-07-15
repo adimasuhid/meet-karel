c03036387227fd64bf0fd47591b0e1f1b475b22c
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-15 19:37:22 +0800

    Add karel

--- test/environment_test.js
+++ test/environment_test.js
@@ -4,23 +4,30 @@ import Environment from '../lib/environment.js'
 describe('Environment', () => {
   describe('#constructor', () => {
     it('creates boulders', () => {
-      const environment = new Environment({ width: 10, height: 5, boulderCount: 3 }) 
+      const environment = new Environment({ width: 10, height: 5, boulderCount: 3 })
 
       assert.equal(environment.boulders().length, 3)
     })
 
-
-    describe('#walls', () => {
+    describe('walls', () => {
       it('creates 8 walls given a 3x3 board', () => {
-        const environment = new Environment({ width: 3, height: 3, boulderCount: 1 }) 
-        
+        const environment = new Environment({ width: 3, height: 3, boulderCount: 1 })
+
         assert.equal(environment.walls().length, 8)
       })
-    
+
       it('creates 14 walls given a 5x4 board', () => {
-        const environment = new Environment({ width: 5, height: 4, boulderCount: 1 }) 
-        
+        const environment = new Environment({ width: 5, height: 4, boulderCount: 1 })
+
         assert.equal(environment.walls().length, 14)
+      })
+    })
+
+    describe('karel', () => {
+      it('creates Karel', () => {
+        const environment = new Environment({ width: 5, height: 4, boulderCount: 1 })
+
+        assert(environment.karel() != null)
       })
     })
   })
