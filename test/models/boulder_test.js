b7825f7d40b0ac55f50a4f00f8ae5bff760b806b
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-17 19:28:51 +0800

    Add pickup and putdown for boulder

--- test/models/boulder_test.js
+++ test/models/boulder_test.js
@@ -9,4 +9,47 @@ describe('Boulder', () => {
       assert(boulder.canBePickedUp())
     })
   })
+
+  describe('#pickUp', () => {
+    it('sets boulder pickup status to true', () => {
+      const boulder = new Boulder({ x: 1, y: 2 })
+      assert(!boulder.isPickedUp())
+
+      boulder.pickUp()
+
+      assert(boulder.isPickedUp())
+    })
+  })
+
+  describe('#putDown', () => {
+    it('places a boulder on a new position', () => {
+      const boulder = new Boulder({ x: 1, y: 2 })
+      const newCoord = { x: 3, y: 4 }
+
+      boulder.pickUp()
+      boulder.putDown(newCoord)
+
+      assert.deepEqual(boulder.coordinates(), newCoord)
+    })
+
+    it('sets boulder to be no longer picked up', () => {
+      const boulder = new Boulder({ x: 1, y: 2 })
+      const newCoord = { x: 3, y: 4 }
+
+      boulder.pickUp()
+      boulder.putDown(newCoord)
+
+      assert(!boulder.isPickedUp())
+    })
+
+    it('does not work when not picked up', () => {
+      const boulder = new Boulder({ x: 1, y: 2 })
+      const newCoord = { x: 3, y: 4 }
+
+      boulder.putDown(newCoord)
+
+      assert.notEqual(boulder.coordinates().x, newCoord.x)
+      assert.notEqual(boulder.coordinates().y, newCoord.y)
+    })
+  })
 })
