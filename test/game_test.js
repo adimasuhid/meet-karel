c488e41de79a4226e5746afaeffdf23b5739cb1e
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-18 21:50:08 +0800

    Add Game

---
+++ test/game_test.js
@@ -0,0 +1,44 @@
+import assert from 'assert'
+import Environment from '../lib/environment.js'
+import Resolver from '../lib/resolver.js'
+import EventLoop from '../lib/event_loop.js'
+import Game from '../lib/game.js'
+
+describe('Game', () => {
+  describe('#resolve', () => {
+    it('runs as a game', (done) => {
+      const eventLoop = new EventLoop({ speed: 5 })
+      const coord = { x: 1, y: 2 }
+      const environment = new Environment({ width: 10, height: 5, boulders: [coord] })
+      const resolver = new Resolver({environment})
+      const game = new Game({resolver, eventLoop})
+      const oldCoord = environment.karel().coordinates()
+
+      game.resolve(function () {
+        this.pickUp()
+        this.move()
+        this.move() // There is a tree in front at (1,4)
+        this.putDown()
+      })
+
+      eventLoop.enqueue(() => {
+        assert(environment.karel().isCarrying())
+      })
+
+      // Going back so I can drop the boulder
+      game.resolve(function () {
+        this.turnLeft()
+        this.turnLeft()
+        this.move()
+        this.turnRight()
+        this.turnRight()
+        this.putDown()
+      })
+
+      eventLoop.enqueue(() => {
+        assert.deepEqual(environment.boulders()[0].coordinates(), { x: 1, y: 3 })
+        done()
+      })
+    })
+  })
+})
