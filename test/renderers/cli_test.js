0c9d9c645486d58d2eadbb3ee0cc3308a97e7f8b
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-19 18:25:18 +0800

    Add initial test

---
+++ test/renderers/cli_test.js
@@ -0,0 +1,17 @@
+import assert from 'assert'
+import Environment from '../../lib/environment.js'
+import Cli from '../../lib/renderers/cli.js'
+
+describe('Renderer.Cli', () => {
+  describe('#render', () => {
+    it('displays the board', () => {
+      const coord = { x: 1, y: 2 }
+      const environment = new Environment({ width: 10, height: 5, boulders: [coord] })
+      const renderer = new Cli({environment})
+
+      renderer.render()
+
+      assert(true)
+    })
+  })
+})
