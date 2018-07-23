a2453cf9da8c40c8c80eff60ef2e1f6c3678f48d
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-23 20:02:53 +0800

    Add webpack for UI rendering

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './lib/app.js',
+  output: {
+    filename: 'karel.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
