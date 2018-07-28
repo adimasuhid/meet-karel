f6650fa1d16efe1415ffeba9bc8f18d1fb84ec33
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-28 20:10:43 +0800

    Add Sprites

--- examples/canvas/setup.js
+++ examples/canvas/setup.js
@@ -1,2 +1,3 @@
 // Setup Karel
-Karel.game.attach(document.getElementById('game'))
+window.Karel.game.attach(document.getElementById('game'))
+window.Karel.game.addAssetSource(document.getElementById('asset-source'))
