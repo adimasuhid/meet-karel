807eb9a4b013786f02a7138960af7d33a6fb87b8
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-29 18:45:27 +0800

    Allow editor to run data directly

--- examples/canvas/setup.js
+++ examples/canvas/setup.js
@@ -2,6 +2,12 @@
 window.Karel.game.attach(document.getElementById('game'))
 window.Karel.game.addAssetSource(document.getElementById('asset-source'))
 
-var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/monokai");
-    editor.session.setMode("ace/mode/javascript");
+window.editor = ace.edit("editor");
+window.editor.setTheme("ace/theme/monokai");
+window.editor.session.setMode("ace/mode/javascript");
+
+document.getElementById("run").addEventListener("click", function () {
+  window.Karel.game.resolve(function () {
+    eval(window.editor.getValue())
+  })
+});
