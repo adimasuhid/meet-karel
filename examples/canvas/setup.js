734c4392c2f6ff09d1cbadd20f6683e1e686cedf
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-29 22:00:31 +0800

    Update dist to pass config to setup

--- examples/canvas/setup.js
+++ examples/canvas/setup.js
@@ -1,23 +1,32 @@
 // Setup Karel
-window.Karel.game.attach(document.getElementById('game'))
-window.Karel.game.addAssetSource(document.getElementById('asset-source'))
+(function () {
+  let eventLoop = new Karel.EventLoop({ speed: 1000 })
+  let coord = { x: 1, y: 2 }
+  let board = new Karel.Board({ width: 15, height: 10, boulders: [coord] })
+  let resolver = new Karel.Resolver({ board: board })
+  let renderer = new Karel.Canvas({ board: board })
+  window.game = new Karel.Game({ resolver: resolver, eventLoop: eventLoop, renderer: renderer })
 
-window.editor = ace.edit("editor");
-window.editor.setTheme("ace/theme/monokai");
-window.editor.session.setMode("ace/mode/javascript");
+  window.game.attach(document.getElementById('game'))
+  window.game.addAssetSource(document.getElementById('asset-source'))
 
-document.getElementById("run").addEventListener("click", function () {
-  try {
-    window.Karel.game.resolve(function () {
-      eval(window.editor.getValue())
-    })
-  } catch (e) {
-    alert(e.message) 
-  }
-});
+  window.editor = ace.edit("editor")
+  window.editor.setTheme("ace/theme/monokai")
+  window.editor.session.setMode("ace/mode/javascript")
 
-document.getElementById("stop").addEventListener("click", function () {
-  window.Karel.game.stop()
-})
+  document.getElementById("run").addEventListener("click", function () {
+    try {
+      window.Karel.game.resolve(function () {
+        eval(window.editor.getValue())
+      })
+    } catch (e) {
+      alert(e.message) 
+    }
+  });
 
-window.Karel.game.render()
+  document.getElementById("stop").addEventListener("click", function () {
+    window.game.stop()
+  })
+
+  window.game.render()
+})()
