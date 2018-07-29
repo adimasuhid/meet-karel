772fc2a480ff0c77b8de3bd6358ffa5be6512159
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-29 22:13:46 +0800

    Add initial stages

---
+++ examples/canvas/stage-4/setup.js
@@ -0,0 +1,31 @@
+// Stage 1
+(function () {
+  let eventLoop = new Karel.EventLoop({ speed: 1000 })
+  let board = new Karel.Board({ width: 15, height: 10, boulders: [] })
+  let resolver = new Karel.Resolver({ board: board })
+  let renderer = new Karel.Canvas({ board: board })
+  window.game = new Karel.Game({ resolver: resolver, eventLoop: eventLoop, renderer: renderer })
+
+  window.game.attach(document.getElementById('game'))
+  window.game.addAssetSource(document.getElementById('asset-source'))
+
+  window.editor = ace.edit("editor")
+  window.editor.setTheme("ace/theme/monokai")
+  window.editor.session.setMode("ace/mode/javascript")
+
+  document.getElementById("run").addEventListener("click", function () {
+    try {
+      window.Karel.game.resolve(function () {
+        eval(window.editor.getValue())
+      })
+    } catch (e) {
+      alert(e.message) 
+    }
+  });
+
+  document.getElementById("stop").addEventListener("click", function () {
+    window.game.stop()
+  })
+
+  window.game.render()
+})()
