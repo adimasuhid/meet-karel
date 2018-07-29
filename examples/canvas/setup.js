fbd9bcffb2c123d7c1adcfe49fa24717f2d70f12
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-07-29 09:32:50 +0800

    Add initial inclusion of editor

--- examples/canvas/setup.js
+++ examples/canvas/setup.js
@@ -1,3 +1,7 @@
 // Setup Karel
 window.Karel.game.attach(document.getElementById('game'))
 window.Karel.game.addAssetSource(document.getElementById('asset-source'))
+
+var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/javascript");
